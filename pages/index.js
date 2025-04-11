a919124f82258b253fd4cec62697d75da3ac4bdb
Neeraj Wadhwani <neeraj.wadhwani@gupshup.io>
2025-04-11 08:41:32 +0530

    add gitignore and files to repo

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: '2rem' }}>
+      <h1> Hello from Next.js on Docker with Ansible!</h1>
+      <p>This is a sample Next.js page deployed using Ansible and Docker.</p>
+    </div>
+  )
+}
